d212148cccb136c9c6a570dd5793e80edff8e874
Miguel-124 <michalgorecki124@gmail.com>
2024-02-17 10:12:52 +0100

    Validation

--- Models/Book.cs
+++ Models/Book.cs
@@ -4,17 +4,22 @@ namespace LibApp.Models
 {
     public class Book
     {
+        [Required(ErrorMessage = "Id is required")]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Title is required")]]
+        [Required(ErrorMessage = "Title is required")]
         [StringLength(100)]
         public string Title { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Author is required")]
         public string Author { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Genre is required")]
         public Genre Genre { get; set; }
+        [Required(ErrorMessage = "GenreId is required")]
         public int GenreId { get; set; }
+        [Required(ErrorMessage = "DateAdded is required")]
         public DateTime DateAdded { get; set; }
+        [Required(ErrorMessage = "Release date is required")]
         public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Number in stock is required")]
         [Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
         public byte NumberInStock { get; set; }
 
