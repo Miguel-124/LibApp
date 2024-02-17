c69c8d62dd741c89cf23642faf118e43f0a70e4f
Miguel-124 <michalgorecki124@gmail.com>
2024-02-17 11:19:42 +0100

    Api1

---
+++ Controllers/API/BooksController.cs
@@ -0,0 +1,107 @@
+﻿using AutoMapper;
+using LibApp.Data;
+using LibApp.Models;
+using LibApp.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibApp.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET /api/books
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            var books = _context.Books
+                .Include(b => b.Genre)
+                .ToList()
+                .Select(_mapper.Map<Book, BookDto>);
+
+            return Ok(books);
+        }
+
+        // GET /api/books/{id}
+        [HttpGet("{id}", Name = "GetBook")]
+        public IActionResult GetBook(int id)
+        {
+            var book = _mapper.Map<BookDto>(_context.Books.Include(b => b.Genre).SingleOrDefault(b => b.Id == id));
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // POST /api/books
+        [HttpPost]
+        public IActionResult CreateBook(BookDto bookDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var book = _mapper.Map<Book>(bookDto);
+            _context.Books.Add(book);
+            _context.SaveChanges();
+
+            return CreatedAtRoute(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+        // PUT /api/books/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, BookDto bookDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var bookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+
+            if (bookInDb == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(bookDto, bookInDb);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE /api/books/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var bookInDb = _context.Books.SingleOrDefault(b => b.Id == id);
+
+            if (bookInDb == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(bookInDb);
+            _context.SaveChanges();
+
+            return Ok(bookInDb);
+        }
+
+
+        private ApplicationDbContext _context;
+        private IMapper _mapper;
+    }
+}
---
+++ Dtos/BookDto.cs
@@ -0,0 +1,30 @@
+﻿using LibApp.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LibApp.Dtos
+{
+    public class BookDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Tittle { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+        [Required]
+        public DateTime? DateAdded { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+        [Required]
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+      
+
+    }
+}
