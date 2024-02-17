314fa4817e40645ea984f778f6a9816024faad0b
Miguel-124 <michalgorecki124@gmail.com>
2024-02-17 09:48:00 +0100

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]]
+        [StringLength(100)]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [Range(1, 20, ErrorMessage = "Number in stock must be between 1 and 20")]
+        public byte NumberInStock { get; set; }
+
+    }
+}
